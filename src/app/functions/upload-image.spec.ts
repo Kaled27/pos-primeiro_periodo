a5ebc78a007906bda6a8722c733d53443de13a14
Romulo Kaled <romulokaled@gmail.com>
2025-11-14 12:05:51 -0300

    feat: aula 15/criado testes de upload de imagem, experando a resposta de sucesso e a resposta se o documento é invalido

---
+++ src/app/functions/upload-image.spec.ts
@@ -0,0 +1,51 @@
+import { Readable } from 'stream'
+import { describe, it, expect, vi, beforeAll } from 'vitest'
+import { uploadImage } from './upload-image'
+import { isLeft, isRight, unwrapEither } from '@/shared/either'
+import { randomUUID } from 'crypto'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { eq } from 'drizzle-orm'
+import { InvalidFileFormat } from './errors/invalide-file-format'
+
+describe('upload image', () => {
+    beforeAll(() => {
+        vi.mock('@/infra/storage/upload-file-to-storage', () => {
+            return {
+                uploadFileToStorage: vi.fn().mockImplementation(() => {
+                    return {
+                        key: `${randomUUID()}.jpg`,
+                        url: 'https://test.com/test.jpg',
+                    }
+                }),
+            }
+        })
+    })   
+
+    it('should be able to upload an image', async () => {
+        const fileName = `${randomUUID()}.jpg`;
+        
+        const sut = await uploadImage({
+            fileName,
+            contentType: 'image/jpeg',
+            contentSteam: Readable.from([]),
+        })
+        expect(isRight(sut)).toBe(true)
+
+        const result = await db.select().from(schema.uploads).where(eq(schema.uploads.name, fileName))
+
+        expect(result).toHaveLength(1)
+    })
+
+    it('should not be able to upload a invalid file', async () => {
+        const fileName = `${randomUUID()}.pdf`;
+        
+        const sut = await uploadImage({
+            fileName,
+            contentType: 'document/pdf',
+            contentSteam: Readable.from([]),
+        })
+        expect(isLeft(sut)).toBe(true)
+        expect(unwrapEither(sut)).toBeInstanceOf(InvalidFileFormat)
+    })
+})
