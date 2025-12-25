c25320c1cf7947a70155dfa284ea6f976696c467
Romulo Kaled <romulo.kaled@brtec.org.br>
2025-12-25 17:29:24 -0300

    fix: ajustado o servico do r2 para atender novas variaveis de ambiente

--- src/infra/storage/client.ts
+++ src/infra/storage/client.ts
@@ -3,9 +3,9 @@ import { S3Client } from '@aws-sdk/client-s3'
 
 export const r2 = new S3Client({
   region: 'auto',
-  endpoint: `https://${env.CLOUDFARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: `https://${env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: env.CLOUDFARE_ACCESS_KEY_ID,
-    secretAccessKey: env.CLOUDFARE_SECRET_ACCESS_KEY,
+    accessKeyId: env.CLOUDFLARE_ACCESS_KEY_ID,
+    secretAccessKey: env.CLOUDFLARE_SECRET_ACCESS_KEY,
   },
 })
