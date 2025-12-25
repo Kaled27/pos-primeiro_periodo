bada279ff9484377e30a02228a9ce27ef4077f04
Romulo Kaled <romulo.kaled@brtec.org.br>
2025-12-25 13:41:16 -0300

    feat aula19/20: utilizando cursores do pg para consultas e também gravando csv do resultado parcial e mandando para o r2

--- src/app/functions/upload-image.spec.ts
+++ src/app/functions/upload-image.spec.ts
@@ -1,51 +1,54 @@
-import { Readable } from 'stream'
-import { describe, it, expect, vi, beforeAll } from 'vitest'
-import { uploadImage } from './upload-image'
-import { isLeft, isRight, unwrapEither } from '@/shared/either'
-import { randomUUID } from 'crypto'
+import { randomUUID } from 'node:crypto'
+import { Readable } from 'node:stream'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { isLeft, isRight, unwrapEither } from '@/shared/either'
 import { eq } from 'drizzle-orm'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
 import { InvalidFileFormat } from './errors/invalide-file-format'
+import { uploadImage } from './upload-image'
 
 describe('upload image', () => {
-    beforeAll(() => {
-        vi.mock('@/infra/storage/upload-file-to-storage', () => {
-            return {
-                uploadFileToStorage: vi.fn().mockImplementation(() => {
-                    return {
-                        key: `${randomUUID()}.jpg`,
-                        url: 'https://test.com/test.jpg',
-                    }
-                }),
-            }
-        })
-    })   
-
-    it('should be able to upload an image', async () => {
-        const fileName = `${randomUUID()}.jpg`;
-        
-        const sut = await uploadImage({
-            fileName,
-            contentType: 'image/jpeg',
-            contentSteam: Readable.from([]),
-        })
-        expect(isRight(sut)).toBe(true)
-
-        const result = await db.select().from(schema.uploads).where(eq(schema.uploads.name, fileName))
-
-        expect(result).toHaveLength(1)
+  beforeAll(() => {
+    vi.mock('@/infra/storage/upload-file-to-storage', () => {
+      return {
+        uploadFileToStorage: vi.fn().mockImplementation(() => {
+          return {
+            key: `${randomUUID()}.jpg`,
+            url: 'https://test.com/test.jpg',
+          }
+        }),
+      }
     })
+  })
 
-    it('should not be able to upload a invalid file', async () => {
-        const fileName = `${randomUUID()}.pdf`;
-        
-        const sut = await uploadImage({
-            fileName,
-            contentType: 'document/pdf',
-            contentSteam: Readable.from([]),
-        })
-        expect(isLeft(sut)).toBe(true)
-        expect(unwrapEither(sut)).toBeInstanceOf(InvalidFileFormat)
+  it('should be able to upload an image', async () => {
+    const fileName = `${randomUUID()}.jpg`
+
+    const sut = await uploadImage({
+      fileName,
+      contentType: 'image/jpeg',
+      contentSteam: Readable.from([]),
     })
+    expect(isRight(sut)).toBe(true)
+
+    const result = await db
+      .select()
+      .from(schema.uploads)
+      .where(eq(schema.uploads.name, fileName))
+
+    expect(result).toHaveLength(1)
+  })
+
+  it('should not be able to upload a invalid file', async () => {
+    const fileName = `${randomUUID()}.pdf`
+
+    const sut = await uploadImage({
+      fileName,
+      contentType: 'document/pdf',
+      contentSteam: Readable.from([]),
+    })
+    expect(isLeft(sut)).toBe(true)
+    expect(unwrapEither(sut)).toBeInstanceOf(InvalidFileFormat)
+  })
 })
